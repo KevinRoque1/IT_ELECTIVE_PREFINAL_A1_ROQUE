ad241b6bbfab856d3df85cc47d9e58bb6422f014
KevinRoque1 <kcndacc@gmail.com>
2026-08-22 09:55:36 +0800

    PREFINAL

---
+++ WebApplication1_roque/WebApplication1_roque/Data/Course.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1_roque.Data
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string CourseCode { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(150)]
+        public string CourseName { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
